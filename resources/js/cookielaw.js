47b212a1082ede4b68fa7661156cbd7cd6f714b1
Tom Wiesing <tkw01536@gmail.com>
2018-05-20 12:23:36 +0200

    Cookie Consent Required

---
+++ resources/js/cookielaw.js
@@ -0,0 +1,73 @@
+/**
+ * This code has been adapted from https://github.com/bmihelac/django-cookie-consent. 
+ * It is licensed as follows: 
+ * 
+ * Copyright (c) 2013, Piotr Kilczuk All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+var Cookielaw = {
+    hasCookie: function(){
+        return this.getCookie('cookielaw_accepted') === '1';
+    },
+
+    getCookie: function(name) {
+        var dc = document.cookie;
+        var prefix = name + "=";
+        var begin = dc.indexOf("; " + prefix);
+        if (begin == -1) {
+            begin = dc.indexOf(prefix);
+            if (begin != 0) return null;
+        }
+        else
+        {
+            begin += 2;
+            var end = document.cookie.indexOf(";", begin);
+            if (end == -1) {
+            end = dc.length;
+            }
+        }
+        // because unescape has been deprecated, replaced with decodeURI
+        //return unescape(dc.substring(begin + prefix.length, end));
+        return decodeURI(dc.substring(begin + prefix.length, end));
+    },
+
+    createCookie: function (name, value, days) {
+        var date = new Date(),
+            expires = '';
+        if (days) {
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    },
+
+    createCookielawCookie: function () {
+        this.createCookie('cookielaw_accepted', '1', 10 * 365);
+
+        if (typeof (window.jQuery) === 'function') {
+            jQuery('#CookielawBanner').slideUp();
+        } else {
+            document.getElementById('CookielawBanner').style.display = 'none';
+        }
+    }, 
+
+    init: function() {
+        if(!Cookielaw.hasCookie()) {
+            document.getElementById('CookielawBanner').style.display = 'block';
+        }
+    }
+
+};
+
+// show the banner if we do not have a cookie
+jQuery(function(){
+    Cookielaw.init();
+})
